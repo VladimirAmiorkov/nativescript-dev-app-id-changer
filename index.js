45c8d200c8916cf162a5755ed8e43e739b4a8ded
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-09-10 17:34:21 +0300

    feat: add script to manipulate NativeScript app id with

---
+++ index.js
@@ -0,0 +1,17 @@
+// Expected args:
+// 1 - path to the NativeScript's package.json file
+// 2 - The unique suffix to add to the app's ID 
+var fs = require('fs');
+
+var myArgs = process.argv.slice(2);
+var jsonPath = myArgs[0];
+var suffix = myArgs[1];
+const appJsonFile = fs.readFileSync(jsonPath);
+var appJson = JSON.parse(appJsonFile);
+var nativeScriptTag = appJson["nativescript"];
+var idTag = nativeScriptTag["id"];
+var newId = idTag + "-" + suffix;
+
+appJson.nativescript.id = newId;
+fs.writeFileSync(jsonPath, JSON.stringify(appJson, null, "\t"));
+console.log("Updated app ID with: ", newId);

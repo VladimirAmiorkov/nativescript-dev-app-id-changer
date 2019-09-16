799cc1f227768c8e13c2189f483a9222cd22b170
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-09-16 13:32:23 +0300

    fix: resovle issue with android builds

--- index.js
+++ index.js
@@ -2,15 +2,42 @@
 // 1 - path to the NativeScript's package.json file
 // 2 - The unique suffix to add to the app's ID 
 var fs = require('fs');
+var pt = require('path')
 
 var myArgs = process.argv.slice(2);
 var jsonPath = myArgs[0];
-var suffix = myArgs[1];
+var onlyPath = pt.dirname(jsonPath);
+
+var appGradlePath = pt.join(onlyPath, "App_Resources", "Android", "app.gradle");
+const appGradleFile = fs.readFileSync(appGradlePath);
+let lines = appGradleFile.toString().split(/(?:\r\n|\r|\n)/g);
+
+var suffix = "." + myArgs[1];
+
+
+var newGradleText;
+lines.forEach((element) => {
+    if (element.includes("applicationId")) {
+        var index = element.lastIndexOf('"');
+        var word = element.substring(0, index);
+        element = word + suffix + '"';
+    }
+    if (newGradleText !== undefined) {
+        newGradleText += element;
+    } else {
+        newGradleText = element;
+    }
+
+    newGradleText += "\r\n";
+});
+
+fs.writeFileSync(appGradlePath, newGradleText);
+
 const appJsonFile = fs.readFileSync(jsonPath);
 var appJson = JSON.parse(appJsonFile);
 var nativeScriptTag = appJson["nativescript"];
 var idTag = nativeScriptTag["id"];
-var newId = idTag + "." + suffix;
+var newId = idTag + suffix;
 
 appJson.nativescript.id = newId;
 fs.writeFileSync(jsonPath, JSON.stringify(appJson, null, "\t"));
